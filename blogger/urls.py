06695c0f25205e2fba56fc1564b242825e01f180
Khasan Musaev <hm.musaev@gmail.com>
2019-11-17 20:05:10 +0000

    Add user page

--- blogger/urls.py
+++ blogger/urls.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from articles import views as article_views
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
     path('articles/', include('articles.urls')),
-    path('', article_views.article_list, name="home")
+    path('', article_views.article_list, name="home"),
+    re_path(r'^@(?P<username>.+)$', views.username_detail)
 ]
--- blogger/views.py
+++ blogger/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from articles.models import Article
 
 
 def homepage(request):
     return render(request, 'homepage.html')
+
+
+def username_detail(request, username):
+    try:
+        username = User.objects.get(username=username)
+    except User.DoesNotExist:
+        username = None
+    articles = Article.objects.filter(author=username)
+    return render(request, 'profile/user_details.html', {'username': username, 'articles': articles})
