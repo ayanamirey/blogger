df18ad2af9c0348741a2104a6e517f740c5444a8
Atabek <atabekmur@gmail.com>
2019-12-02 22:05:15 +1100

    Add basic templates

--- blogger/urls.py
+++ blogger/urls.py
@@ -1,4 +1,6 @@
+from django.conf import settings
 from django.contrib import admin
+from django.conf.urls.static import static
 from django.urls import path, include, re_path
 from articles import views as article_views
 from . import views
@@ -9,4 +11,4 @@ urlpatterns = [
     path('articles/', include('articles.urls')),
     path('', article_views.article_list, name="home"),
     re_path(r'^@(?P<username>.+)$', views.username_detail)
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
