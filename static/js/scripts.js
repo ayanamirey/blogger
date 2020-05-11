ff1e08ec01f1788d5a752e8f747eac5cf1d91fff
ikhasanmusaev <hm.musaev@gmail.com>
2020-05-11 22:27:02 +0500

    fixed upload image

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -123,20 +123,18 @@
 
 })(jQuery);
 
-$(document).ready(function () {
-    var readURL = function (input) {
-        if (input.files && input.files[0]) {
-            var reader = new FileReader();
-            reader.onload = function (e) {
-                $('.profile-pic').attr('src', e.target.result);
-            }
-            reader.readAsDataURL(input.files[0]);
+let readURL = function (input) {
+    if (input.files && input.files[0]) {
+        let reader = new FileReader();
+        reader.onload = function (e) {
+            $('.profile-pic').attr('src', e.target.result);
         }
+        reader.readAsDataURL(input.files[0]);
     }
-    $(".file-upload").on('change', function () {
-        readURL(this);
-    });
-    $(".upload-button").on('click', function () {
-        $(".file-upload").click();
-    });
-});
+}
+$(".file-upload").change(function () {
+    readURL(this);
+})
+$(".upload-button").click(function () {
+    $(".file-upload").click();
+})
