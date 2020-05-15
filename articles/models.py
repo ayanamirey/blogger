6809b6e61ad11dc00846a103fd78c49e99d09d72
Khasan Musaev <hm.musaev@gmail.com>
2020-05-15 11:07:08 +0000

    Calculate the time to read an article and add it to templates

--- articles/models.py
+++ articles/models.py
@@ -2,6 +2,7 @@ from django.core.validators import RegexValidator
 from django.db import models
 from django.contrib.auth.models import User
 from markdownx.models import MarkdownxField
+import re
 
 from core.models import TimestampedModel
 
@@ -34,3 +35,17 @@ class Article(TimestampedModel):
 
     def snippet_large(self):
         return self.body[:350] + '...'
+
+    def time_of_read(self):
+        t1 = len(self.body) / 1500
+        t2 = len([m.start() for m in re.finditer("(?:!\[(.*?)\]\((.*?)\))", self.body)])
+        the_sum = t1 + t2
+        n = int(the_sum * 10) % 10
+        if the_sum < 1:
+            result = '1'
+        else:
+            if n >= 3:
+                result = str(int(the_sum) + 1)
+            else:
+                result = str(the_sum)
+        return result + ' min'
