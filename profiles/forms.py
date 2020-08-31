d365bc7eae90ef6688c09ee5918ef74a9cb6df36
Khasan Musaev <hm.musaev@gmail.com>
2020-08-31 11:46:13 +0000

    Allow changing username from profile

--- profiles/forms.py
+++ profiles/forms.py
@@ -6,10 +6,20 @@ from profiles.models import Profile
 class UserForm(forms.ModelForm):
     class Meta:
         model = User
-        fields = ('first_name', 'last_name', 'email')
+        fields = ('username', 'first_name', 'last_name', 'email')
+
+    last_name = forms.CharField(required=False)
+    username = forms.CharField(min_length=3)
 
 
 class ProfileForm(forms.ModelForm):
     class Meta:
         model = Profile
         fields = ('bio', 'location', 'birth_date')
+
+    bio = forms.Textarea()
+    location = forms.CharField(required=False)
+    birth_date = forms.DateField(required=False,
+                                 widget=forms.DateInput(format='%d-%m-%Y'),
+                                 input_formats=('%d-%m-%Y',), help_text='Hint to date DD-MM-YYYY',
+                                 )
